1d5f6dcceebbc9d72fa8b697b1b4064f98ae49d0
Iiquids <83255875+Iiquids@users.noreply.github.com>
2022-05-17 18:02:06 -0400

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,117 @@
+var form = document.getElementById('srchbutton');
+var input = document.getElementById('srchinput');
+
+form.addEventListener('click', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+        if ((localStorage.getItem('aboutBlankCloaking')) === 'true') {
+            var urle = "https://" + document.domain + __uv$config.prefix + __uv$config.encodeUrl(url);
+            win = window.open();
+            win.document.body.style.margin = '0';
+            win.document.body.style.height = '100vh';
+            var iframe = win.document.createElement('iframe');
+            iframe.style.border = 'none';
+            iframe.style.width = '100%';
+            iframe.style.height = '100%';
+            iframe.style.margin = '0';
+            iframe.src = urle;
+            console.log(iframe.src)
+            win.document.body.appendChild(iframe)
+
+        } else {
+            window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        }
+    });
+});
+
+function openUrl(url) {
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        if (url == "pornhub.com") url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        if (url == "xvideo.com") url =
+            'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        if (url == "canary.giggl.app") url =
+            'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        if (url == "wattpad.com") url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+        if ((localStorage.getItem('aboutBlankCloaking')) === 'true') {
+            var urle = "https://" + document.domain + __uv$config.prefix + __uv$config.encodeUrl(url);
+            win = window.open();
+            win.document.body.style.margin = '0';
+            win.document.body.style.height = '100vh';
+            var iframe = win.document.createElement('iframe');
+            iframe.style.border = 'none';
+            iframe.style.width = '100%';
+            iframe.style.height = '100%';
+            iframe.style.margin = '0';
+            iframe.src = urle;
+            console.log(iframe.src)
+            win.document.body.appendChild(iframe)
+
+        } else {
+            window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        }
+    });
+}
+
+function isUrl(val = '') {
+    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    return false;
+};
+
+// Games ~~
+
+function discord() {
+    openUrl("https://www.nvidia.com/en-us/geforce-now/")
+}
+
+function spotify() {
+    openUrl("https://open.spotify.com/")
+}
+
+function robloxLink() {
+    openUrl("https://now.gg/play/roblox-corporation/5349/roblox")
+}
+function retroBowl() {
+    openUrl('https://retrobowlgame.com/')
+}
+function poki() {
+    openUrl("https://poki.com/")
+}
+function minecraft() {
+    openUrl("https://blockcraft.victorwei.com/")
+}
+function geforceNow() {
+    openUrl("https://play.geforcenow.com")
+}
+
+function v1lol() {
+    openUrl("https://1v1.lol/")
+}
+
+function amoungUs() {
+    openUrl("https://now.gg/play/innersloth-llc/4047/among-us")
+}
+
+function youtube() {
+    openUrl("https://youtube.com")
+}
+
+function twitch() {
+    openUrl("https://twitch.tv")
+}
+
+function tiktok() {
+    openUrl("https://tiktok.com")
+}
+
+function animixPlay() {
+    openUrl("https://animixplay.to")
+}
